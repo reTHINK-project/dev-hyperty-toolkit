b7375f98f4b0989b08283d86cdd1dec956dd24ea
Vitor Silva <vitorsilvaemail@gmail.com>
2016-09-27 18:13:47 +0100

    Update the version of hyperties, protostubs and others resources

--- Gulpfile.js
+++ Gulpfile.js
@@ -163,40 +163,67 @@ gulp.task('server', function(done) {
 
         res.setHeader('Access-Control-Allow-Origin', '*');
 
-        if (req.originalUrl.includes('.well-known')) {
-          if (req.originalUrl.includes('version')) {
-            res.writeHeader(200, {'Content-Type': 'text/plain'});
-            res.write('1.0');
-            res.end();
-          } else {
-            var paths = req.originalUrl.split('/');
-            var type = paths[2];
-            var resource = paths[3];
+        var paths;
 
-            var raw;
-            switch (type) {
-              case 'runtime':
-                raw = JSON.parse(fs.readFileSync('./resources/descriptors/Runtimes.json', 'utf8'));
+        if (req.originalUrl.includes('.well-known')) {
+
+          paths = req.originalUrl.split('/');
+          var type = paths[2];
+          var resource = paths[3];
+
+          if (req.originalUrl.includes('sourcepackage')) {
+            paths = req.originalUrl.split('/');
+            var cguid = Number(paths[3]);
+            var idType = cguid.toString().substring(0, 1);
+            var sourcePackage;
+            var selectedObject;
+            var resourceObject;
+
+            switch (idType) {
+              case '1':
+                resourceObject = getResources('hyperty');
+                selectedObject = filterResource(resourceObject, cguid);
+                sourcePackage = resourceObject[selectedObject].sourcePackage;
                 break;
-              case 'hyperty':
-                raw = JSON.parse(fs.readFileSync('./resources/descriptors/Hyperties.json', 'utf8'));
+
+              case '2':
+                resourceObject = getResources('dataschema');
+                selectedObject = filterResource(resourceObject, cguid);
+                sourcePackage = resourceObject[selectedObject].sourcePackage;
                 break;
-              case 'idp-proxy':
-                raw = JSON.parse(fs.readFileSync('./resources/descriptors/IDPProxys.json', 'utf8'));
+
+              case '3':
+                sourcePackage = getResources('runtime');
+                selectedObject = filterResource(resourceObject, cguid);
+                sourcePackage = resourceObject[selectedObject].sourcePackage;
                 break;
-              case 'protocolstub':
-                raw = JSON.parse(fs.readFileSync('./resources/descriptors/ProtoStubs.json', 'utf8'));
+
+              case '4':
+                resourceObject = getResources('protocolstub');
+                selectedObject = filterResource(resourceObject, cguid);
+                sourcePackage = resourceObject[selectedObject].sourcePackage;
                 break;
-              case 'dataschema':
-                raw = JSON.parse(fs.readFileSync('./resources/descriptors/DataSchemas.json', 'utf8'));
+              case '5':
+                sourcePackage = getResources('idp-proxy');
+                selectedObject = filterResource(resourceObject, cguid);
+                sourcePackage = resourceObject[selectedObject].sourcePackage;
                 break;
             }
+
+            res.writeHeader(200, {'Content-Type': 'application/json'});
+            res.end(JSON.stringify(sourcePackage));
+
+          } else {
+
+            var raw = getResources(type);
 
             res.writeHeader(200, {'Content-Type': 'application/json'});
             if (resource) {
 
               if (req.originalUrl.includes('cguid')) {
-                res.end(JSON.stringify(raw[resource].cguid));
+                res.end(JSON.stringify(Number(raw[resource].cguid), '', 2));
+              } else if (req.originalUrl.includes('version')) {
+                res.end(JSON.stringify(Number(raw[resource].version), '', 2));
               } else {
                 res.end(JSON.stringify(raw[resource], '', 2));
               }
@@ -229,6 +256,35 @@ gulp.task('server', function(done) {
   });
 
 });
+
+function getResources(type) {
+  var raw;
+  switch (type) {
+    case 'runtime':
+      raw = JSON.parse(fs.readFileSync('./resources/descriptors/Runtimes.json', 'utf8'));
+      break;
+    case 'hyperty':
+      raw = JSON.parse(fs.readFileSync('./resources/descriptors/Hyperties.json', 'utf8'));
+      break;
+    case 'idp-proxy':
+      raw = JSON.parse(fs.readFileSync('./resources/descriptors/IDPProxys.json', 'utf8'));
+      break;
+    case 'protocolstub':
+      raw = JSON.parse(fs.readFileSync('./resources/descriptors/ProtoStubs.json', 'utf8'));
+      break;
+    case 'dataschema':
+      raw = JSON.parse(fs.readFileSync('./resources/descriptors/DataSchemas.json', 'utf8'));
+      break;
+  }
+
+  return raw;
+}
+
+function filterResource(resource, key) {
+  return Object.keys(resource).filter(function(a) {
+    return resource[a].cguid === key;
+  })[0];
+}
 
 gulp.task('environment', function() {
 
@@ -326,7 +382,9 @@ gulp.task('watch', function(done) {
     var fileObject = path.parse(event.path);
     return gulp.src([fileObject.dir + '/*.hy.js'])
     .pipe(convertHyperty())
+    .resume()
     .on('end', function() {
+      console.log('testes');
       browserSync.reload();
     });
   });
@@ -793,7 +851,12 @@ function encode(opts) {
     });
 
     json[value].type = opts.descriptor;
-    json[value].version = '0.1';
+
+    // json[value].version = 0.1;
+    if (json[value].version) {
+      json[value].version = Number(json[value].version) + 0.1;
+    }
+
     json[value].description = checkValues('description', 'Description of ' + filename, json[value]);
     json[value].objectName = checkValues('objectName', filename, json[value]);
 
