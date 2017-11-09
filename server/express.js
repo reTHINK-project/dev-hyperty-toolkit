410e2a2bc44d896f172fe1ec46f746d3fb03e039
Vitor Silva <vitor-t-silva@telecom.pt>
2017-11-09 11:36:56 +0000

    Fix the descriptors messageSchema string to array

--- server/express.js
+++ server/express.js
@@ -30,9 +30,6 @@ app.use(express.static(process.env.HYPERTY_REPO + '/' + config.templates + '/'))
 
 app.use('/.well-known/runtime/', express.static(process.cwd() + '/node_modules/runtime-browser/bin/'));
 
-// app.use('/.well-known/runtime/sandbox.html', express.static(process.cwd() + '/node_modules/runtime-browser/bin/sandbox.html'));
-// app.use('/.well-known/runtime/index.html', express.static(process.cwd() + '/node_modules/runtime-browser/bin/index.html'));
-
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
